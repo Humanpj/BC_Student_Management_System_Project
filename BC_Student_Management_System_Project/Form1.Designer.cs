f4d9c5cbab55f08e9a32e8e0358f658a4313e392
HumanPetrusPJ <577842@student.belgiumcampus.ac.za>
2024-10-29 09:31:04 +0200

    Basic Gui created

--- BC_Student_Management_System_Project/Form1.Designer.cs
+++ BC_Student_Management_System_Project/Form1.Designer.cs
@@ -28,20 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.btnViewAllStudents = new System.Windows.Forms.Button();
+            this.btnUpdateStudent = new System.Windows.Forms.Button();
+            this.btnDeleteStudent = new System.Windows.Forms.Button();
+            this.btnGenerateSummary = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(25, 12);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(75, 23);
+            this.btnAddStudent.TabIndex = 0;
+            this.btnAddStudent.Text = "Add Student";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnViewAllStudents
+            // 
+            this.btnViewAllStudents.Location = new System.Drawing.Point(106, 12);
+            this.btnViewAllStudents.Name = "btnViewAllStudents";
+            this.btnViewAllStudents.Size = new System.Drawing.Size(99, 23);
+            this.btnViewAllStudents.TabIndex = 1;
+            this.btnViewAllStudents.Text = "View All Students";
+            this.btnViewAllStudents.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateStudent
+            // 
+            this.btnUpdateStudent.Location = new System.Drawing.Point(211, 12);
+            this.btnUpdateStudent.Name = "btnUpdateStudent";
+            this.btnUpdateStudent.Size = new System.Drawing.Size(146, 23);
+            this.btnUpdateStudent.TabIndex = 2;
+            this.btnUpdateStudent.Text = "Update Student Information";
+            this.btnUpdateStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteStudent
+            // 
+            this.btnDeleteStudent.Location = new System.Drawing.Point(363, 12);
+            this.btnDeleteStudent.Name = "btnDeleteStudent";
+            this.btnDeleteStudent.Size = new System.Drawing.Size(99, 23);
+            this.btnDeleteStudent.TabIndex = 3;
+            this.btnDeleteStudent.Text = "Delete a student";
+            this.btnDeleteStudent.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerateSummary
+            // 
+            this.btnGenerateSummary.Location = new System.Drawing.Point(468, 12);
+            this.btnGenerateSummary.Name = "btnGenerateSummary";
+            this.btnGenerateSummary.Size = new System.Drawing.Size(122, 23);
+            this.btnGenerateSummary.TabIndex = 4;
+            this.btnGenerateSummary.Text = "Generate a summary";
+            this.btnGenerateSummary.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(695, 12);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(75, 23);
+            this.btnLogout.TabIndex = 5;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(25, 58);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.Size = new System.Drawing.Size(611, 312);
+            this.dgvStudents.TabIndex = 6;
             // 
             // frmStudentManagementSystem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnGenerateSummary);
+            this.Controls.Add(this.btnDeleteStudent);
+            this.Controls.Add(this.btnUpdateStudent);
+            this.Controls.Add(this.btnViewAllStudents);
+            this.Controls.Add(this.btnAddStudent);
             this.Name = "frmStudentManagementSystem";
             this.Text = "Student Management System";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Button btnViewAllStudents;
+        private System.Windows.Forms.Button btnUpdateStudent;
+        private System.Windows.Forms.Button btnDeleteStudent;
+        private System.Windows.Forms.Button btnGenerateSummary;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.DataGridView dgvStudents;
     }
 }
 
