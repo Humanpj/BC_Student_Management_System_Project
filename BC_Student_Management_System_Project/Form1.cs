1677100f34bc4e6180bcb4527d95ea7f0ef0f570
HumanPetrusPJ <577842@student.belgiumcampus.ac.za>
2024-10-29 09:23:48 +0200

    Form created and uploaded.

--- BC_Student_Management_System_Project/Form1.cs
+++ BC_Student_Management_System_Project/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BC_Student_Management_System_Project
 {
-    public partial class Form1 : Form
+    public partial class frmStudentManagementSystem : Form
     {
-        public Form1()
+        public frmStudentManagementSystem()
         {
             InitializeComponent();
         }
--- BC_Student_Management_System_Project/Program.cs
+++ BC_Student_Management_System_Project/Program.cs
@@ -16,7 +16,7 @@ namespace BC_Student_Management_System_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmStudentManagementSystem());
         }
     }
 }
